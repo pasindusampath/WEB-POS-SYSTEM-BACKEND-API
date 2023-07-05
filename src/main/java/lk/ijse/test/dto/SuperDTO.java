46ec4f9f9af7fdb14e09e3d8b79020903f87fd21
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-05 19:01:51 +0530

    Created Customer DTO for implement encapsulation within project

--- src/main/java/lk/ijse/test/dto/SuperDTO.java
+++ src/main/java/lk/ijse/test/dto/SuperDTO.java
@@ -1,4 +1,6 @@
 package lk.ijse.test.dto;
 
-public interface SuperDTO {
+import java.io.Serializable;
+
+public interface SuperDTO extends Serializable {
 }
--- src/main/java/lk/ijse/test/dto/custom/CustomerDTO.java
+++ src/main/java/lk/ijse/test/dto/custom/CustomerDTO.java
@@ -1,6 +1,7 @@
 package lk.ijse.test.dto.custom;
 
 
+import lk.ijse.test.dto.SuperDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CustomerDTO {
+public class CustomerDTO implements SuperDTO {
     private int id;
     private String name;
     private String address;
