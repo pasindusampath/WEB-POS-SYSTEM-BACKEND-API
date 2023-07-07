2848d024ab0405338f7710fd0748023792987b02
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-07 11:45:20 +0530

    Added Item As An Entity in Hibernate Factory Configuration

--- src/main/java/lk/ijse/test/db/FactoryConfiguration.java
+++ src/main/java/lk/ijse/test/db/FactoryConfiguration.java
@@ -1,6 +1,7 @@
 package lk.ijse.test.db;
 
 import lk.ijse.test.entity.custome.Customer;
+import lk.ijse.test.entity.custome.Item;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,7 +10,8 @@ public class FactoryConfiguration {
     private static FactoryConfiguration instance;
     private SessionFactory factory;
     private FactoryConfiguration(){
-        Configuration configure = new Configuration().configure().addAnnotatedClass(Customer.class);
+        Configuration configure = new Configuration().configure().addAnnotatedClass(Customer.class).
+                addAnnotatedClass(Item.class);
         factory=configure.buildSessionFactory();
     }
 
