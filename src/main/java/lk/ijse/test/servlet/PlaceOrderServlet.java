3b840d7871dc82277638285551753f44062579ef
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-09 22:42:43 +0530

    Added Conversion to convert receiving data from placing order

--- src/main/java/lk/ijse/test/servlet/PlaceOrderServlet.java
+++ src/main/java/lk/ijse/test/servlet/PlaceOrderServlet.java
@@ -1,5 +1,10 @@
 package lk.ijse.test.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+import lk.ijse.test.tm.CartTM;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 @WebServlet(name = "placeOrder",urlPatterns = "/place")
 public class PlaceOrderServlet extends HttpServlet {
@@ -19,5 +26,13 @@ public class PlaceOrderServlet extends HttpServlet {
             builder.append(data);
         }
         System.out.println(builder);
+
+        Type listType = new TypeToken<List<CartTM>>() {}.getType();
+
+        List<CartTM> yourList = new Gson().fromJson(builder.toString(), listType);
+        for (CartTM ob : yourList){
+            System.out.println(ob);
+        }
+
     }
 }
