fc3570ae308cb195c829ebfb853b6e6cba11fe85
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-07-23 06:31:27 +0530

    Added Filter To Fix CORS Policy Problem

---
+++ src/main/java/lk/ijse/test/filter/HttpFilter.java
@@ -0,0 +1,25 @@
+package lk.ijse.test.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "HttpFilter" ,urlPatterns = "/*")
+public class HttpFilter extends javax.servlet.http.HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String origin = req.getHeader("Origin");
+        System.out.println(origin);
+        if(origin!=null && origin.contains("http://localhost")){
+            res.setHeader("Access-Control-Allow-Origin", req.getHeader("Origin"));
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        }
+        chain.doFilter(req,res);
+    }
+
+}
